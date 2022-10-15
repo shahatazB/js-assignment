a77d38f3dd89aa93565358d87a00a78ef62ea565
shahatazb <shahatazbegum7@gmail.com>
2022-10-15 15:58:57 +0530

    fundamentals of js

---
+++ index.js
@@ -0,0 +1,179 @@
+// alert('this is java script fundamental class');
+ //console.log('logging output in console');
+ //document.write('writing data on web page');
+
+ //var a;
+ //var a=100;
+ //console.log(a);
+
+ //var a=100,b='100',c=false,d=null,e;
+
+ //console.log(a,b,c,d,e);
+
+ //console.log(typeof(b));
+
+ //Arithmetic operators;
+// +,-,*,/,%
+
+ //var a=20;
+ //var b=10;
+// var c=a*b;
+ //console.log(c);
+
+// comparision operators;
+ //<,>,<=,>=,!=,==,===
+
+ //var a=10, b=20;
+ //console.log(a>=b);
+
+//logical operators;
+// &&,||,!
+
+ //var a=10, b=20;
+ //console.log(a<b || a<5);
+  //console.log(!a==b);
+
+  //Array
+
+   //var arr=[10,'abc',true,null,undefined];
+
+    //console.log(arr);
+   //console.log(arr[1]);
+   //console.log(arr.length);
+   //console.log(arr[10]);
+ 
+
+   //how to store data outside array
+    //var newArr=[];
+    //newArr[0]=10;
+    //newArr[2]='hello';
+   //console.log(newArr);
+
+    //object
+
+    //var obj={
+   //a:10,
+   //b:'hello',
+   //c:true,
+   //d:null,
+   //e:undefined,
+   //f:[10,'100',true,null,undefined]
+    //}
+
+
+
+    //console.log(obj);
+    //[]
+    //console.log(obj['c']);
+   //console.log(obj.d);
+    //console.log(obj.f[3]);
+    //obj.g ='new property';
+    //console.log(obj);
+
+    //var newobj={};
+    //newobj.a=10;
+    //newobj.b='hello';
+    //console.log(newobj);
+
+    //function
+    //syntax
+    //functon funtionname(parameterlist){
+     // statements
+    //}
+
+    //function add(){
+      //var a=10;
+      //var b=20;
+      //var c=a+b;
+      //console.log(c);
+    //}
+    //add();
+    //function sumtwonumb(a,b){
+      //a=10,b=20;
+      //var c=a+b;
+      //console.log(c);
+    //}
+    //sumtwonumb();
+    //sumtwonumb(20,30);
+    
+  //   function calculator(a,b,operator){
+    // switch(operator) {
+      // case'+': console.log(a+b);
+       //break;
+       //case'-':console.log(a-b);
+       //break;
+       //case'*': console.log(a*b);
+       //break;
+       //case'/': console.log(a/b);
+       //break;
+       //default:console.log("nothing");
+    
+     //} 
+      //}
+
+      //calculator(30,20,'*');
+      //if(3>9){
+       // console.log("yes");
+      //}
+      //else{
+        //console.log("no");
+      //}
+      //var numb=100;
+      //switch(numb) {
+        //case 200:
+          //console.log("1000");
+          //break;
+         // default: console.log("2000");
+           // break;
+          
+      //}
+      //for( var i=10;i>=10;i++){
+        //console.log(i);
+
+      //}
+      //var a=10;
+     // var b=a++;
+      //var c=++a;
+
+      //console.log(a,b,c);
+      //console.log(a++);
+      //console.log(++a);
+
+
+      //var j=20;
+
+      //while(j<50){
+        //console.log(j);
+        //j++;
+      //}
+      //do{
+        //++j;
+        //console.log(j);
+      //} while(j<30);
+
+      //helper methods of array in js
+
+      //var colors=['pink','white','rose','blue'];
+      //colors.push('yellow');// adds an item at the end of array
+      //colors.pop();//removes an item at the end of the array
+      //colors.shift();//removes 0th index element
+      //colors.unshift('red');//add 0th index at starting point of an array
+      //console.log(colors);
+
+
+      //slice and splice 
+      // slice is used to remove elements to array
+       //var students=['mubeen','husna','nida','fatima'];
+       //console.log(students);
+       // var newstudents=students.slice(3,4);
+       //console.log(newstudents);
+
+       //splice is used to add new elements to array
+       // var fruits=['banana','orange','mango'];
+       //fruits.splice(4,0,'badam','pista');
+       // console.log(fruits);
+       g
+
+
+
+
